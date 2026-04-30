e1aa994da8148714d0b39fe233077e2035e62451
copeihsinoc <peihsinev01@gmail.com>
2026-04-30 14:57:47 +0800

    catPhoto

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -54,7 +54,13 @@ export class SignupComponent implements OnInit {
     'assets/cat3.png',
     'assets/cat4.png',
     'assets/cat5.png',
-    'assets/cat6.png'
+    'assets/cat6.png',
+    'assets/cat7.png',
+    'assets/cat8.png',
+    'assets/cat9.png',
+    'assets/cat10.png',
+    'assets/cat11.png',
+    'assets/cat12.png'
   ];
 
   constructor(
