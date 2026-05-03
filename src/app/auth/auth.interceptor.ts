9eaad4c47e4cb67ccae13e7b0f389fb69a202a46
copeihsinoc <peihsinev01@gmail.com>
2026-05-03 20:48:09 +0800

    security

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -7,8 +7,8 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = sessionStorage.getItem('authToken');
     
-    // 🔍 檢查這個日誌有沒有在瀏覽器 Console 出現
-    console.log('🚀 [Interceptor] 攔截器啟動！請求網址:', req.url);
+    
+    console.log('🚀 [Interceptor] Requete sortante vers:', req.url);
 
     if (token) {
       const cloned = req.clone({
@@ -17,7 +17,7 @@ export class AuthInterceptor implements HttpInterceptor {
       return next.handle(cloned);
     }
     
-    console.warn('⚠️ [Interceptor] 警告：在 sessionStorage 找不到 authToken');
+    console.warn('⚠️ [Interceptor] Aucun jeton trouvé dans le sessionStorage');
     return next.handle(req);
   }
 }
